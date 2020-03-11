0c85a098c8c0e7763041988bc914c3e33652fc27
david115599 <david.bershadsky2@gmail.com>
2020-03-10 20:05:26 -0400

    n

--- server.js
+++ server.js
@@ -25,7 +25,6 @@ app.get('/', function(request, response){
   response.render('index',{feedback:""});
 });
 
-
 app.post('/view', function(request, responsea, body){
   var options = {
     url: 'http://apps.who.int/gho/athena/api/GHO/WHOSIS_000001',
